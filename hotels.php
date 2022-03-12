e0256e1d9de7ae1271b2a7998bf0179f9a54f2ab
Amit12345p <94756804+Amit12345p@users.noreply.github.com>
2022-03-12 06:27:15 +0530

    Add files via upload

---
+++ hotels.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <title>Document</title>
+    <style>
+        body{
+            background-image: url('images/13.jpg');
+        } 
+    </style>
+</head>
+<body>
+    
+<?php include('config/constants.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Food Order System</title>
+
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<section id="tutorials" class="text-center">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 col-md-10 ml-auto mr-auto btn-danger">
+        <div class="section-title">
+          <b><h2 class="title">Awesome <span>Hotels</span></h2></b>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4 col-md-6">
+        <div class="featured-item">
+          <div class="service-num">
+            <span>01</span>
+          </div>
+          <div class="post post-hover" data-bg-img="images//hotel//gg.jpg">
+          <div class="featured-icon">
+            <i class="flaticon-responsive"></i>
+          </div>
+          <div class="featured-title text-uppercase">
+            <li class="nav-item">
+              <h4><a class="nav-link text-dark btn-success" href="#"> <span>Fuchhey Hotel</span></h4>
+                <p><b>Best Food</b></p>
+              </a>
+              <a class="nav-link btn-info" target="_blank" href="http://localhost/onlinefood-order/category-foods.php?category_id=4"><b>visit here
+              </a></b>
+              </a>
+              </li>
+          </div>
+          </div>
+          <div class="featured-desc">
+          </div>         
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 sm-mt-5">
+        <div class="featured-item">
+          <div class="service-num">
+            <span>02</span>
+          </div>
+          <div class="post post-hover" data-bg-img="images/aa.jpg">
+          <div class="featured-icon">
+            <i class="flaticon-seo"></i>
+          </div>
+          <div class="featured-title text-uppercase">
+            <li class="nav-item">
+              <h4><a class="nav-link text-dark btn-success" href="#"> <span>Kumari Hotel & Bar</span></h4>
+                <p><b>Multi Cuisine</b></p>
+              </a>
+              <a class="nav-link btn-info" target="_blank" href="http://localhost/onlinefood-order/category-foods.php?category_id=4"><b>visit here
+              </a></b>
+              </a>
+              </li>
+          </div>
+          </div>
+          <div class="featured-desc">
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 md-mt-5">
+        <div class="featured-item">
+          <div class="service-num">
+            <span>03</span>
+          </div>
+          <div class="post post-hover" data-bg-img="images/dd.jpg">
+          <div class="featured-icon">
+            <i class="flaticon-responsive"></i>
+          </div>
+          <div class="featured-title text-uppercase">
+            <li class="nav-item">
+              <h4><a class="nav-link text-dark btn-success" href="#"> <span>Gupta Hotel</span></h4>
+                <p><b>Nepali Food</b></p>
+              </a>
+              <a class="nav-link btn-info" target="_blank" href="http://localhost/onlinefood-order/category-foods.php?category_id=4"><b>visit here
+              </a></b>
+              </a>
+              </li>
+          </div>
+          </div>
+          <div class="featured-desc">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+</body>
+</html>
